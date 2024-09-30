c1ca4d3e11c61f6990c32fe5bea4c3da25ccfa79
Vinicius Prestes <viniciusnevesprestes@gmail.com>
2024-09-30 15:28:29 -0300

    Criando a HomePage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
-import Carousel from '../components/Carousel';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { HomeContainer } from '../components/StyledComponents';
 
 const Home = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    setUser(null);
+    navigate('/');
+  };
+
   return (
-    <div>
-      <h2>Novidades</h2>
-      <Carousel />
-      <section>
-        <h2 id="formula-e">Conhecendo a Formula E</h2>
-        {/* Conteúdo sobre a Fórmula E */}
-      </section>
-    </div>
+    <HomeContainer>
+      {user ? (
+        <>
+          <h1>Bem-vindo, {user.email}!</h1>
+          <button onClick={handleLogout}>Sair</button>
+        </>
+      ) : (
+        <>
+          <h1>Bem-vindo à página de esportes!</h1>
+          <Link to="/login">Entrar</Link>
+        </>
+      )}
+    </HomeContainer>
   );
 };
 
